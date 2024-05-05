039881d6f6efb872d62efa6b0771499fe95a22cb
DSRN0000 <dsrn0000@gmail.com>
2024-05-05 16:58:42 +0900

    report1

---
+++ src/report1/Report1.java
@@ -0,0 +1,47 @@
+package report1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Report1 {
+
+	public static void main(String[] args) {
+		int n = 1;
+		int start = 0, end = 99;
+		Random ran = new Random();
+		int random = ran.nextInt(100); //0 ~ 99까지 랜덤값 생성
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("수를 결정했습니다. 맞추어 보세요");
+		System.out.println(start + "-" + end);
+		
+		while(true) {
+			System.out.print(n + ">>");
+			int num = sc.nextInt();
+			
+			if (num == random) {
+				System.out.println("맞았습니다.");
+				System.out.print("다시하시겠습니까(y/n)>>");
+				String contin = sc.next();
+				
+				if (contin.equals("y")) {
+					continue;
+				}else if (contin.equals("n")){
+					break;
+				}
+			}else if (num < random){
+				int tmp = start;
+				start = num;
+				System.out.println("더 높게");
+				System.out.println(start + "-" + end);
+			}else {
+				int tmp2 = end;
+				end = num;
+				System.out.println("더 낮게");
+				System.out.println(start + "-" + end);
+			}
+		}
+
+	}
+
+}
